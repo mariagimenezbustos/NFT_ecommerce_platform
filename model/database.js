62108df3e99a2e154e5ec3330c8e5844cf5f3524
mariagimenezbustos <134734638+mariagimenezbustos@users.noreply.github.com>
2023-10-20 17:43:47 +0200

    heroku prep

--- model/database.js
+++ model/database.js
@@ -6,21 +6,24 @@ const DB_HOST = process.env.DB_HOST;
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
+const STACKHERO_MYSQL_ROOT_PASSWORD = process.env.STACKHERO_MYSQL_ROOT_PASSWORD;
+const STACKHERO_MYSQL_HOST = process.env.STACKHERO_MYSQL_HOST;
 
 const con = mysql.createConnection({
-  host: DB_HOST || "127.0.0.1",
+  host: STACKHERO_MYSQL_HOST, // DB_HOST || "127.0.0.1",
   user: DB_USER || "root",
-  password: DB_PASS,
+  password: STACKHERO_MYSQL_ROOT_PASSWORD, // DB_PASS,
   database: DB_NAME || "ecommerce",
-  multipleStatements: true
+  multipleStatements: true,
+  ssl: {},
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 
   let sql = fs.readFileSync(__dirname + "/init_db.sql").toString();
-  con.query(sql, function(err, result) {
+  con.query(sql, function (err, result) {
     if (err) throw err;
     console.log(`Your database ${DB_NAME} was successfuly created!`);
 
